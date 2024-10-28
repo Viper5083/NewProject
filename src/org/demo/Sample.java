3f98ec1c605f79c882ce428d090ec5a0095a1fd8
Viper5083 <mohamedashif00929@gmail.com>
2024-10-28 13:18:31 +0530

    done

---
+++ src/org/demo/Sample.java
@@ -0,0 +1,32 @@
+package org.demo;
+
+public class Sample {
+
+	private void axis() {
+		System.out.println("Axis Acount is available");
+
+	}
+	private void cub() {
+		System.out.println("City Union Bank is available");
+
+	}
+	
+	private void indian() {
+		System.out.println("Indian Bank is available");
+
+	}
+	public static void main(String[] args) {
+		
+		Sample s = new Sample();
+		s.axis();
+		s.cub();
+		s.indian();
+		
+		
+		
+		
+		
+		
+		
+	}
+}
